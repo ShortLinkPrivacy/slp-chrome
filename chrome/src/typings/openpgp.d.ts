090eb9e4b16e5d0dcf17759dd2877f4e2ff3cc06
Stefan Geneshky <stefanguen@gmail.com>
2015-11-17 07:10:08 -0800

    Update reference path to openpgp.d.ts

--- chrome/src/typings/openpgp.d.ts
+++ chrome/src/typings/openpgp.d.ts
@@ -3,7 +3,7 @@
 // Definitions by: Guillaume Lacasa <https://blog.lacasa.fr>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
-/// <reference path="../es6-promise/es6-promise.d.ts" />
+/// <reference path="../../typings/es6-promise/es6-promise.d.ts" />
 
 declare module openpgp {
 
