d3c426eb882119b812336cf5893a011ff07dafec
Stefan Geneshky <stefanguen@gmail.com>
2015-11-19 17:31:57 -0800

    Add my own typings for rivets

---
+++ chrome/src/typings/rivets-tests.ts
@@ -0,0 +1,27 @@
+/// <reference path="rivets.d.ts" />
+
+rivets.configure({
+    // Attribute prefix in templates
+    prefix: 'rv',
+    // Preload templates with initial data on bind
+    preloadData: true,
+    // Root sightglass interface for keypaths
+    rootInterface: '.',
+    // Template delimiters for text bindings
+    templateDelimiters: ['[[', ']]'],
+    // Augment the event handler of the on-* binder
+    handler: function(target:any, event:any, binding:any) {
+        this.call(target, event, binding.view.models)
+    }
+});
+
+var t = {test: ["hello", "one", "two"]}
+var opts = {bar: "foo"};
+rivets.bind(document.getElementById("para1"), t);
+rivets.bind(document.getElementById("para1"), t, opts);
+rivets.bind([document.getElementById("para1"), document.getElementById("para2")], t);
+
+rivets.binders["foo"] = 1;
+rivets.components["foo"] = 1;
+rivets.formatters["foo"] = 1;
+rivets.adapters["foo"] = 1;
