8fa48a84e1a2396ef10c0ce7d0381956c85031f4
Stefan Geneshky <stefanguen@gmail.com>
2015-12-22 18:51:24 -0800

    Break content into smaller pieces

--- chrome/src/content/main.ts
+++ chrome/src/content/main.ts
@@ -1,8 +1,5 @@
-/// <reference path="../typings/openpgp/openpgp.d.ts" />
-/// <reference path="../typings/rivets/rivets.d.ts" />
-/// <reference path="../typings/chrome/chrome.d.ts" />
-/// <reference path="modules.d.ts" />
-
+/// <reference path="popup.ts" />
+/// <reference path="armor.ts" />
 
 var config = new Config();
 
@@ -13,9 +10,6 @@ var privateKey: Keys.PrivateKey;
 
 // Contains all loaded modules
 var loadedModules: Interfaces.Dictionary = {};
-
-// Regexp for a PGP message
-var pgpRe = /----BEGIN PGP MESSAGE----/gi;
 
 // Nodes to decrypt
 var nodes: Array<Node>;
@@ -43,13 +37,15 @@ function loadModule(name: string, callback: Interfaces.Callback): void {
 function nodesToDecrypt(): Array<Node> {
     var walk: TreeWalker,
         node: Node,
-        result: Array<Node>;
+        result: Array<Node>,
+        armorType: number;
 
     result = [];
     walk = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT);
 
     while (node = walk.nextNode()) {
-        if ( node.nodeValue.match(pgpRe) ) {
+        armorType = getArmorType(node.nodeValue);
+        if ( armorType == 3 || armorType == 4 ) {   // TODO: messages only
             result.push(node);
         }
     }
@@ -66,22 +62,18 @@ function nodesToDecrypt(): Array<Node> {
  *********************************************************/
 function processNodes(nodes: Array<Node>): void {
     var node: Node,
-        i: number,
-        text: string;
+        i: number;
 
     for (i = 0; i < nodes.length; i++) {
         node = nodes[i];
-        text = node.nodeValue;
-        if (text.match(pgpRe)) {
-            var message = openpgp.message.readArmored(text);
-            openpgp.decryptMessage( privateKey.key, message )
-               .then((plainText) => {
-                   node.nodeValue = plainText;
-               })
-               .catch((error) => {
-                   console.log(error);
-               });
-        }
+        var message = openpgp.message.readArmored(node.nodeValue);
+        openpgp.decryptMessage( privateKey.key, message )
+           .then((plainText) => {
+               node.nodeValue = plainText;
+           })
+           .catch((error) => {
+               node.nodeValue = "&lt;PGP MESSAGE&gt;"; // TODO: icon
+           });
     }
 }
 
@@ -148,214 +140,6 @@ function run(): void {
             }
         }
     });
-}
-
-class Popup {
-
-    /*------------------------------------------------------------
-     *  Private variables and constants
-     *-----------------------------------------------------------*/
-
-    // Icon to show in the corner of each textarea
-    private iconSize: number = 16;
-    private iconSrc: string = chrome.runtime.getURL("/images/padlock.png");
-
-    private popupWidth = 300;
-    private popupHeight = 300;
-
-    // Iframe to open inside the popup
-    private iframeSrc = chrome.runtime.getURL("/iframe.html");
-
-    // Textarea props to save (and restore) when showing and hiding the icon in
-    // the corner
-    private propsToSave: Array<string> = [
-        'backgroundPositionX',
-        'backgroundPositionY',
-        'backgroundRepeat',
-        'backgroundAttachment',
-        'backgroundImage'
-    ];
-
-
-    // Save element's props here
-    private savedProps: Object;
-
-    /*------------------------------------------------------------
-     *  Public variables and constructor
-     *-----------------------------------------------------------*/
-
-    // Holder of the currently opened UI object
-    static current: Popup;
-
-    // The texarea element we're modifying
-    el: HTMLTextAreaElement;
-
-    // The popup div that will contain the iframe
-    popup: HTMLElement;
-
-    // The iframe element
-    iframe: HTMLIFrameElement;
-
-    // A this-bound version of closePopup
-    closeBound: EventListenerObject;
-
-    constructor(el: HTMLTextAreaElement) {
-        this.el = el;
-        this.savedProps = {};
-
-        // Save the current props
-        this.saveProps();
-
-        // Bind events
-        el.addEventListener('mousemove', this.onMouseMove.bind(this));
-        el.addEventListener('mouseout', this.onMouseOut.bind(this));
-        el.addEventListener('keydown', this.onKeyDown.bind(this));
-        el.addEventListener('click', this.onClick.bind(this));
-
-        // Bind closer
-        this.closeBound = this.closePopup.bind(this);
-    }
-
-    /*------------------------------------------------------------
-     *  Event handers
-     *-----------------------------------------------------------*/
-
-    private onMouseMove(e: MouseEvent): void {
-        if (!this.canEncrypt()) return;
-        this.showIcon();
-        if ( this.isOverIcon(e) ) {
-            this.el.style.cursor = 'pointer';
-        } else {
-            this.el.style.cursor = 'text';
-        }
-    }
-
-    private onMouseOut(e: MouseEvent): void {
-        this.hideIcon();
-    }
-
-    private onKeyDown(e: Event): void {
-        this.hideIcon();
-    }
-
-    private onClick(e: MouseEvent): void {
-        if (!this.canEncrypt()) return;
-        if ( this.isOverIcon(e) ) this.openPopup(e.x, e.y);
-    }
-
-    /*------------------------------------------------------------
-     *  Save and restore element props
-     *-----------------------------------------------------------*/
-
-    // Save the element's style properties
-    saveProps(): void {
-        this.propsToSave.forEach((p) => {
-            this.savedProps[p] = this.el.style[p]
-        })
-    }
-
-    // Restore saved properties
-    restoreProps(): void {
-        this.propsToSave.forEach((p) => {
-            this.el.style[p] = this.savedProps[p]
-        })
-    }
-
-    /*------------------------------------------------------------
-     *  Icon
-     *-----------------------------------------------------------*/
-
-    isOverIcon(e: MouseEvent): boolean {
-        return this.el.offsetWidth - e.offsetX < this.iconSize && e.offsetY < this.iconSize
-    }
-
-    canEncrypt(): boolean {
-        if (!this.el.value) return false;
-        if (this.el.value.match(/localhost/)) return false; // TODO: real url
-        return true;
-    }
-
-    showIcon(): void {
-        this.el.style.backgroundPositionX = 'right'
-        this.el.style.backgroundPositionY = 'top'
-        this.el.style.backgroundRepeat = 'no-repeat'
-        this.el.style.backgroundAttachment = 'scroll'
-        this.el.style.backgroundImage = "url(" + this.iconSrc + ")"
-    }
-
-    hideIcon(): void {
-        this.restoreProps()
-    }
-
-    /*------------------------------------------------------------
-     *  Open and close popup
-     *-----------------------------------------------------------*/
-
-    openPopup(x: number, y: number): void {
-
-        var popup: HTMLElement,
-            iframe: HTMLIFrameElement;
-
-        // popup element
-        popup = document.createElement('div')
-        popup.style.width = this.popupWidth + "px"
-        popup.style.height = this.popupHeight + "px"
-        popup.style.position = "absolute"
-        popup.style.left = x + "px"
-        popup.style.top = y + "px"
-
-        // iframe element
-        iframe = document.createElement('iframe')
-        iframe.src = this.iframeSrc
-        iframe.width = "100%"
-        iframe.height = "100%"
-        iframe.style.border = "none"
-
-        popup.appendChild(iframe)
-        document.body.appendChild(popup)
-
-
-        // If clicked on the popup, do not bubble down
-        popup.addEventListener('click', (e: Event) => {
-            e.stopPropagation()
-        });
-
-
-        // If clicked outside of the popup, close it.
-        // Can't bind right away, because it fires on the icon click
-        setTimeout(() => {
-            document.addEventListener('click', this.closeBound);
-        }, 1000);
-
-        this.popup = popup
-        this.iframe = iframe
-        Popup.current = this
-    }
-
-    // Close the popup and optionally set the new text of the input
-    closePopup(newText?: string): void {
-        this.popup.remove();
-        Popup.current = null;
-        document.removeEventListener('click', this.closeBound);
-        if ( newText ) {
-            this.el.value = newText;
-            this.el.dispatchEvent(new Event('input'));
-        }
-    }
-
-    encrypt(armoredPublicKeys: Array<string>, callback: Interfaces.ResultCallback): void {
-        var keys: Array<openpgp.key.Key>;
-
-        loadModule("openpgp", () => {
-            keys = armoredPublicKeys.map((armoredText: string) => {
-                return openpgp.key.readArmored(armoredText).keys[0];
-            })
-
-            openpgp.encryptMessage( keys, this.el.value ).then(callback).catch((err) => {
-                console.log(err);
-            })
-        });
-    }
 }
 
 window.onload = function() {
