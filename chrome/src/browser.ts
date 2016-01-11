6d5cd4e1bc43a00d7342c55f7197dbd09e18834d
Stefan Geneshky <stefanguen@gmail.com>
2016-01-10 20:00:07 -0800

    Merge articles

--- chrome/src/browser.ts
+++ chrome/src/browser.ts
@@ -1,17 +1,69 @@
 /// <reference path="../typings/openpgp/openpgp.d.ts" />
 /// <reference path="../typings/rivets/rivets.d.ts" />
 /// <reference path="../typings/chrome/chrome.d.ts" />
-/// <reference path="../typings/pathjs/pathjs.d.ts" />
 /// <reference path="modules.d.ts" />
 
 var app: App,
     bg: Interfaces.BackgroundPage = <Interfaces.BackgroundPage>chrome.extension.getBackgroundPage();
 
+//---------------------------------------------------------------------------
+// Sends messages to the content script of the current tab
+//---------------------------------------------------------------------------
 function sendMessageToContent(msg: any, callback?: Interfaces.ResultCallback): void {
     chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
         chrome.tabs.sendMessage(tabs[0].id, msg, callback);
     });
 }
+
+//---------------------------------------------------------------------------
+// Encrypt own private key and create a crypted url
+//---------------------------------------------------------------------------
+function encryptPrivateKey(callback: Interfaces.SuccessCallback): void {
+    var armoredText: string;
+
+    // TODO: cache this link in the settings
+    armoredText = bg.privateKey.toPublic().armored();
+
+    bg.messageStore.save(armoredText, (result) => {
+        if ( result.success ) {
+            callback({
+                success: true,
+                value: bg.messageStore.getURL(result.id)
+            })
+        } else {
+            callback({
+                success: false,
+                error: result.error
+            })
+        }
+    });
+}
+
+function encryptMessage(text: string, keyList: Array<openpgp.key.Key>, callback: Interfaces.SuccessCallback): void {
+    openpgp.encryptMessage( keyList, text )
+        .then((armoredText) => {
+            bg.messageStore.save(armoredText, (result) => {
+                if ( result.success ) {
+                    callback({
+                        success: true,
+                        value: bg.messageStore.getURL(result.id)
+                    });
+                } else {
+                    callback({
+                        success: false,
+                        error: result.error
+                    });
+                }
+            });
+        })
+        .catch((err) => {
+            callback({
+                success: false,
+                error: "OpenPGP Error: " + err
+            });
+        });
+}
+
 
 /*
  * Address Book tab controller
@@ -24,34 +76,50 @@ function KeyItem(k) {
   }
 }
 
+
 /*
-class KeyItem {
-    key: Keys.PublicKey;
+ * The main application handles all articles, bit it itself
+ * also handles the private key password entry screen.
+ */
 
-    constructor(key: Keys.PublicKey) {
-        this.key = key;
-    }
-
-    getPrimaryUser(): string {
-        return this.key.getPrimaryUser();
-    }
+interface BoolFunc {
+    (): boolean;
 }
-*/
 
-class EncryptTab implements Application.Article {
+class App {
 
-    filename = "encrypt.html";
-    articleId = "encrypt";
+    error: string;
+    password: string;
+
+    hasPrivateKey: BoolFunc;
+    isDecrypted: BoolFunc;
+    hasSelectedKeys: BoolFunc;
+    hasFoundKeys: BoolFunc;
+
     filter: string;
     foundKeys = [];
     selectedKeys = [];
     clearText: string;
     alreadyEncrypted: boolean;
-    hasSelectedKeys: { (): boolean } = function() { return this.selectedKeys.length > 0 };
-    hasFoundKeys: { (): boolean } = function() { return this.foundKeys.length > 0 };
 
     constructor() {
         this.filter = ""; // TODO - last used
+
+        this.hasPrivateKey = function() {
+            return bg.privateKey ? true : false;
+        }
+
+        this.isDecrypted = function() {
+            return this.hasPrivateKey() ? bg.privateKey.isDecrypted() : false;
+        }
+
+        this.hasSelectedKeys = function() {
+            return this.selectedKeys.length > 0
+        };
+
+        this.hasFoundKeys = function() {
+            return this.foundKeys.length > 0
+        };
 
         sendMessageToContent({ getElement: true }, (el) => {
             var re = new RegExp(bg.messageStore.getReStr());
@@ -62,6 +130,9 @@ class EncryptTab implements Application.Article {
         });
     }
 
+    //---------------------------------------------------------------------------
+    // Execute the search for public keys
+    //---------------------------------------------------------------------------
     doFilter(): void {
         if ( !this.filter ) {
             this.foundKeys = [];
@@ -85,44 +156,27 @@ class EncryptTab implements Application.Article {
         return false;
     }
 
-    select(e: Event, model: {index: number}) {
+    //---------------------------------------------------------------------------
+    // Add the clicked key to the list of selected keys
+    //---------------------------------------------------------------------------
+    addPublicKey(e: Event, model: {index: number}) {
         var item = this.foundKeys[model.index];
         if ( this.isSelected(item) == false ) {
             this.selectedKeys.push(item);
         }
     }
 
-    remove(e: Event, model: {index: number}) {
+    //---------------------------------------------------------------------------
+    // Remove the clicked key from the list of selected keys
+    //---------------------------------------------------------------------------
+    removePublicKey(e: Event, model: {index: number}) {
         this.selectedKeys.splice(model.index, 1);
     }
 
-    private encryptMessage(text: string, keyList: Array<openpgp.key.Key>, callback: Interfaces.SuccessCallback): void {
-
-        openpgp.encryptMessage( keyList, text )
-            .then((armoredText) => {
-                bg.messageStore.save(armoredText, (result) => {
-                    if ( result.success ) {
-                        callback({
-                            success: true,
-                            value: bg.messageStore.getURL(result.id)
-                        });
-                    } else {
-                        callback({
-                            success: false,
-                            error: result.error
-                        });
-                    }
-                });
-            })
-            .catch((err) => {
-                callback({
-                    success: false,
-                    error: "OpenPGP Error: " + err
-                });
-            });
-    }
-
-    submit(e: Event) {
+    //---------------------------------------------------------------------------
+    // Encrypt the message and paste the url back to the textarea
+    //---------------------------------------------------------------------------
+    sendMessage(e: Event) {
         var keyList: Array<openpgp.key.Key>;
 
         // This should never happen because we don't show the submit button
@@ -133,142 +187,32 @@ class EncryptTab implements Application.Article {
         // Also push our own key, so we can read our own message
         keyList.push(bg.privateKey.key.toPublic());
 
-        this.encryptMessage(this.clearText, keyList, (result) => {
+        encryptMessage(this.clearText, keyList, (result) => {
             if ( result.success ) {
                 sendMessageToContent({ setElement: result.value });
                 window.close();
             } else {
-                app.error = result.error;
+                this.error = result.error;
             }
         })
     }
 
-    restore(e: Event) {
+    //---------------------------------------------------------------------------
+    // Restore the original message back in the textarea
+    //---------------------------------------------------------------------------
+    restoreMessage(e: Event) {
         sendMessageToContent({ restore: true }, (result) => {
             if ( result.success ) {
                 window.close();
             } else {
-                app.error = "There was an error";
+                this.error = "There was an error";
             }
         })
     }
 
-    goSettings(e: MouseEvent): void {
-        e.preventDefault();
-        app.goSettings(e);
-    }
-}
-
-class MyKeyTab implements Application.Article {
-    filename = "mykey.html";
-    articleId = "myKey";
-    publicKey: string;
-
-    private encryptKey(sendResponse: Interfaces.SuccessCallback): void {
-        var armoredText: string;
-
-        // TODO: cache this link in the settings
-        armoredText = bg.privateKey.toPublic().armored();
-
-        bg.messageStore.save(armoredText, (result) => {
-            if ( result.success ) {
-                sendResponse({
-                    success: true,
-                    value: bg.messageStore.getURL(result.id)
-                })
-            } else {
-                sendResponse({
-                    success: false,
-                    error: result.error
-                })
-            }
-        });
-    }
-
-    submit(): void {
-        this.encryptKey((result) => {
-            if ( result.success ) {
-                sendMessageToContent({ setElement: result.value });
-                window.close();
-            } else {
-                app.error = result.error;
-            }
-        })
-    }
-}
-
-class LockTab implements Application.Article {
-    filename = "lock.html";
-    articleId = "lock";
-
-    submit(): void {
-        bg.privateKey.lock();
-
-        chrome.tabs.query({currentWindow: true}, (tabs) => {
-            tabs.forEach((tab) => {
-                chrome.tabs.sendMessage(tab.id, { lock: true });
-            });
-        });
-
-        window.close();
-    }
-}
-
-/*
- * The main application handles all articles, bit it itself
- * also handles the private key password entry screen.
- */
-
-class App extends Application.Main {
-    error: string;
-    password: string;
-    tabs: any = {};
-    initVars: Interfaces.InitVars;
-
-    constructor( config: Application.AppConfig ) {
-        super(config);
-
-        // Articles
-        this.registerArticle( new EncryptTab() );
-        this.registerArticle( new MyKeyTab() );
-        this.registerArticle( new LockTab() );
-
-        // Router
-        this.router();
-    }
-
-    router(): void {
-        Path.map("#/a").to(() => { this.loadArticle('encrypt') });
-        Path.map("#/b").to(() => { this.loadArticle('myKey') });
-        Path.map("#/c").to(() => { this.loadArticle('control') });
-        Path.map("#/d").to(() => { this.loadArticle('lock') });
-    }
-
-    // Use the DOM API to manipulate the tabs. Why not rivets?  Because
-    // rivets doesn't allow for calculation within the HTML, so stuff
-    // like rv-class-active="currentTab == 'blah'" doesn't fly.
-    activateTab(name: string): void {
-        var elements: HTMLCollection,
-            i: number;
-
-        elements = document.getElementById('tabs').children;
-        for (i = 0; i < elements.length; i++) {
-            var li: Element,
-                a: Element;
-
-            li = elements[i];
-            a = (<HTMLElement>li).children[0];
-            a.className = a.getAttribute('rel') == name ? "active" : "";
-        }
-    }
-
-    // Overload the parent loadArticle, so we can also activate the tab
-    // for that article.
-    loadArticle(articleId: string, onBindArgs?: any): void {
-        super.loadArticle(articleId, onBindArgs);
-        this.activateTab(articleId);
-    }
-
+    //---------------------------------------------------------------------------
+    // Unlock the private key by providing a password
+    //---------------------------------------------------------------------------
     enterPassword(e: KeyboardEvent): void {
         if ( e.keyCode != 13 ) {
             this.error = "";
@@ -290,38 +234,57 @@ class App extends Application.Main {
         }
     }
 
+    //---------------------------------------------------------------------------
+    // Lock the privateKey
+    //---------------------------------------------------------------------------
+    lock(): void {
+        bg.privateKey.lock();
+
+        chrome.tabs.query({currentWindow: true}, (tabs) => {
+            tabs.forEach((tab) => {
+                chrome.tabs.sendMessage(tab.id, { lock: true });
+            });
+        });
+
+        window.close();
+    }
+
+    //---------------------------------------------------------------------------
+    // Encrypt own private key and pastes the url to the textarea
+    //---------------------------------------------------------------------------
+    sendPrivateKey(): void {
+        encryptPrivateKey((result) => {
+            if ( result.success ) {
+                sendMessageToContent({ setElement: result.value });
+                window.close();
+            } else {
+                this.error = result.error;
+            }
+        })
+    }
+
+    //---------------------------------------------------------------------------
+    // Go to the settings page
+    //---------------------------------------------------------------------------
     goSettings(e: MouseEvent): void {
         chrome.runtime.openOptionsPage(() => {});
     }
 
+    //---------------------------------------------------------------------------
+    // Run application
+    //---------------------------------------------------------------------------
     run(): void {
-        this.initVars = bg.initialize();
-
-        // Rivets
         rivets.configure({
             handler: function(target, ev, binding) {
                 this.call(app, ev, binding.view.models)
             }
         });
         rivets.bind(document.body, this);
-
-        // Path
-        Path.listen();
-
-        // Init
-        if (this.initVars.isDecrypted) {
-            window.location.hash = "#/a";
-        }
     }
 }
 
 window.onload = function() {
-    app = window["app"] = new App({
-        path: "src/templates/browser"
-    });
-
+    app = window["app"] = new App();
     app.run();
 };
-
-
 
