28dd4b78b9d7e60d35f47da521590deeba72a7ad
Stefan Geneshky <stefanguen@gmail.com>
2016-03-20 16:30:29 -0700

    Update MagicURL class

--- chrome/src/modules/url.ts
+++ chrome/src/modules/url.ts
@@ -1,24 +1,43 @@
-class MagicUrl {
-    static reStr = "https?://slp.li/(\d)/([0-9,a-f]+)";
-    static re = new RegExp(MagicUrl.reStr);
-    static reG = new RegExp(MagicUrl.reStr, "i");
+class MagicURL {
+    static domain = "https?://slp.li";
+    static anyPath = "m|k";
+    static messagePath = "m";
+    static keyPath = "k";
+    static id = "[0-9,a-f]+";
 
     path: string;
     id: string;
 
-    constructor(url: text) {
-        var m = url.match(MagicUrl.re);
+    constructor(url: string) {
+        var m = url.match(MagicURL.anyRegExp());
         if ( !m ) throw "no_match";
         
         this.path = m[1];
         this.id = m[2];
     }
 
+    private static genericRe(path: string, flags?: string): RegExp {
+        // Trap the path in ()s so it becomes a capture
+        return new RegExp(MagicURL.domain + "/(" + path + ")/(" + MagicURL.id + ")", flags);
+    }
+
+    static anyRegExp(flags?: string): RegExp {
+        return MagicURL.genericRe(MagicURL.anyPath, flags);
+    }
+
+    static messageRegExp(flags?: string): RegExp {
+        return MagicURL.genericRe(MagicURL.messagePath, flags);
+    }
+
+    static keyRegExp(flags) {
+        return MagicURL.genericRe(MagicURL.keyPath, flags);
+    }
+
     isMessage(): boolean {
-        return this.path == "m";
+        return this.path == MagicURL.messagePath;
     }
 
     isKey(): boolean {
-        return this.path == "k";
+        return this.path == MagicURL.keyPath;
     }
 }
